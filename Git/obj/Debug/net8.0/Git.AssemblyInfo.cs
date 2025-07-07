4a60e5e0fd9831340dfcf4e8423d6e1f5a98eb07
Supriya-Jonnala <supriya.jonnalasj@gmail.com>
2025-07-07 15:29:33 +0530

    Adding new class1 and modified program.cs

--- Git/obj/Debug/net8.0/Git.AssemblyInfo.cs
+++ Git/obj/Debug/net8.0/Git.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Git")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4610010efdd4a361a9c788a0d7f983416976793")]
 [assembly: System.Reflection.AssemblyProductAttribute("Git")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Git")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
